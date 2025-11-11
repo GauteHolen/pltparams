bca05ff5721952320799627437dff5a9b3fcd223
GauteHolen <gaute.ah@gmail.com>
2025-11-11 12:58:23 +0100

    init commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+   name='pltparams',
+   version='0.1',
+   packages=find_packages(),
+   license='MIT',
+   author='Gaute Holen',
+   author_email='gaute.ah@gmail.com',
+   long_description=open('README.md').read(),
+   url='https://github.com/GauteHolen/pltparams',
+)

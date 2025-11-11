5961a1ec99be60fb8b389104a0bebcf8214faa46
GauteHolen <gaute.ah@gmail.com>
2025-11-11 15:58:13 +0100

    update example and readme

--- readme.md
+++ readme.md
@@ -1,6 +1,16 @@
 # Scientific style for matplotlib on import
 
+The default font, font size, figure size, color shceme, linewidth, marker size etc. are not suited for scientific publications. This package sets the defaults to suitable parameters on import, and offers some handy functions for setting new defaults or cycling props.
+
 ## Install
+
+Use pip package
+```bash
+pip install pltparams
+```
+
+Or download the repo to your computer
+
 ```bash
 cd path/to/pltparams
 pip install .
@@ -21,6 +31,53 @@ import pltparams
 pltparams.set_params()
 ```
 
+The defualt colorscheme has some hard to see colors for white backgrounds, especially if it defaults to the darkmode theme. The `'tab10'`colorshcme is good for white background, and is defualt here. Call `use_tab10_color` to revert back to it. 
+
+For black and white, we call `use_classic_bw_style()` for different linestyles and markers instead of color. This is probably the most useful command.
+
+There's also an alternate `use_okabe_ito_color()` if you prefer the okabe ito color scheme.
+
+Cycle through the markers by simply calling `next(pltparams.marker_cycle)`. This is the easiest way, as setting default markers will also impose them on `plot()`in addition to `scatter()`. 
+
+```python
+# Plot single column (Default style )
+fig, ax = plt.subplots(1,1,figsize = pltparams.figsize_single)
+plot_example(ax)
+
+```
+![Example single column plot](figures/example_single_default.png)
+
+For the "classic" black and white style
+```python
+pltparams.use_classic_bw_lines()
+fig, ax = plt.subplots(1,1,figsize = pltparams.figsize_single)
+plot_example(ax)
+```
+
+![Classic BW](figures/example_single_bw.png)
+
+Alternate okabe ito color scheme
+```python
+pltparams.use_okabe_ito_color()
+fig, ax = plt.subplots(1,1,figsize = pltparams.figsize_single)
+plot_example(ax)
+```
+![Okabe Ito](figures/example_single_okabe_it.png)
+
+## Cycle props
+We can fetch the next marker using `next(marker_cycle)`, to easily grab the next markers. This is the easiest way to do this, as marker prop cycles can not be set in the defualt RCParams.
+```python
+ax.scatter(x_scatter, y_scatter, marker = next(pltparams.marker_cycle), label="data 1 scatter")
+```
+
+There's also a `letter_labels_lc` and `letter_labels_uc` for lower case, and upper case, letters. This is handy for subplots.
+```python
+ax.set_title(pltparams.letter_labels_lc[j]+") : "+subplot_labels[j], loc='left')
+```
+![Subplots](figures/example_single_subplots.png)
+
+
+# Alternatively copy paste this
 
 These are the params the package will change, feel free to copy/paste if you prefer that
 ```python
@@ -56,3 +113,4 @@ params = {"ytick.color" : "black",
 plt.rcParams.update(params)
 plt.rcParams['figure.constrained_layout.use'] = True
 ```
+
--- src/pltparams/pltparams.py
+++ src/pltparams/pltparams.py
@@ -1,15 +1,60 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from cycler import cycler
+import itertools
 
-figsize = np.array([85,85]) #Figure size in mm for double column width in mm
-figsize_double = np.array([160,85]) #Figure size in mm for single column width in mm
+# Fricking freedom units smh
+mm_to_inch = 1 / 25.4
+
+double_column_width = 85 / 25.4  #inches
+single_column_width = 160 / 25.4  #inches
+
+
+figsize_double = np.array([double_column_width,85 * mm_to_inch]) #Figure size in inches for double column width in mm
+figsize_single = np.array([single_column_width,85 * mm_to_inch]) #Figure size in inches for single column width in mm
+golden_ratio = 0.618
+
+figsize_double_golden = np.array([double_column_width, double_column_width / golden_ratio])#Figure size in inches for double column width in mm
+figsize_single_golden = np.array([single_column_width, single_column_width * golden_ratio]) #Figure size in inches for single column width in mm
+
+figsize_double_fullpage = np.array([double_column_width,200 * mm_to_inch]) #Figure size in inches for full page width in mm
+figsize_single_fullpage = np.array([single_column_width,200 * mm_to_inch]) #Figure size in inches for full page width in mm
 dpi = 300                         #Print resolution
 ppi = np.sqrt(3456**2+2234**2)/24 #Screen resolution
 #########################
 
 
+markers = ['o', 'x', '+', '^', '*', 's', 'D', 'v', 'P', 'H']
+marker_cycle = itertools.cycle(markers)
 
-params = {"ytick.color" : "black",
+letter_labels_lc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't']
+letter_labels_uc = [letter.upper() for letter in letter_labels_lc]
+
+def set_params():
+    plt.rcParams.update(params)
+    plt.rcParams['figure.constrained_layout.use'] = True
+
+def use_classic_bw_lines():
+    colors = ['black'] 
+    linestyles = ['-', '--', '-.', ':']
+    plt.rcParams['axes.prop_cycle'] = cycler(color=colors) * cycler(linestyle=linestyles)
+
+def use_default_lines():
+    plt.rcParams['axes.prop_cycle'] = plt.rcParamsDefault['axes.prop_cycle']
+
+def use_okabe_ito_color():
+    okabe_ito = ['#E69F00', '#56B4E9', '#009E73', '#F0E442', 
+             '#0072B2', '#D55E00', '#CC79A7', '#999999']
+    plt.rcParams['axes.prop_cycle'] = cycler(color=okabe_ito)
+
+def use_tab10_color():
+    colors = plt.get_cmap('tab10').colors
+    plt.rcParams['axes.prop_cycle'] = cycler(color=colors)
+
+
+params = {
+          "lines.color" :"black",
+          "ytick.color" : "black",
           "xtick.color" : "black",
           "axes.labelcolor" : "black",
           "axes.edgecolor" : "black",
@@ -21,14 +66,15 @@ params = {"ytick.color" : "black",
           "xtick.labelsize": 10,
           "ytick.labelsize": 10,
           "legend.fontsize": 7,
-          "figure.figsize": figsize/ 25.4, #Convert from mm to inches
+          "figure.figsize": figsize_double, 
           'figure.constrained_layout.use': True,
           'figure.constrained_layout.hspace': 0.01,
           'figure.constrained_layout.wspace': 0.01,
           'figure.constrained_layout.h_pad':  0.02167,
           'figure.constrained_layout.w_pad':  0.02167,
           #'figure.autolayout': True,
-          'lines.linewidth' : 0.5,
+          'lines.linewidth' : 0.8,
+          'lines.markersize' : 4,
           'figure.facecolor' : "white",
           'axes.facecolor' : "white",
           'text.color' : 'black',
@@ -38,9 +84,7 @@ params = {"ytick.color" : "black",
           'savefig.format' : 'pdf',
 
           }
+
 plt.rcParams.update(params)
 plt.rcParams['figure.constrained_layout.use'] = True
-
-def set_params():
-    plt.rcParams.update(params)
-    plt.rcParams['figure.constrained_layout.use'] = True
+use_tab10_color()
